ffa00a1b850c242fbd89e964ff58407a5f0eeb68
Jasmin Kaset <kasetski@gmail.com>
2021-02-09 13:57:54 -0600

    server events

--- Levelup.session.sql
+++ Levelup.session.sql
@@ -3,3 +3,4 @@ SELECT * FROM authtoken_token;
 SELECT * FROM levelupapi_gamer;
 SELECT * FROM levelupapi_game;
 SELECT * FROM levelupapi_event;
+SELECT * FROM levelupapi_game_type;
--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -2,3 +2,6 @@ from .auth import login_user
 from .auth import register_user
 from .game_type import GameTypes
 from .game import Games
+from .game import GameSerializer
+from .event import Events
+
