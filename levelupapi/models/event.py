584bf90c53280160a0f29e7d908086154950da47
Jasmin Kaset <kasetski@gmail.com>
2021-02-15 07:17:31 -0600

    gamer can join or leave an event, custom actions added to views

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -8,5 +8,12 @@ class Event(models.Model):
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     location = models.CharField(max_length=50)
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
     
    
